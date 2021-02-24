5b8ca08a1dd40d1eacdff50dfafd04b3f6556fe2
Nishant Mourya <2019kuec2010@iiitkota.ac.in>
2021-02-24 17:50:27 +0530

    updated sections

--- js/custom.js
+++ js/custom.js
@@ -33,9 +33,9 @@ $(document).ready(function() {
   $(function() {
     $('body').vegas({
         slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
+            { src: 'images/slide-img1.webp' },
+            { src: 'images/slide-img2.webp' },
+            { src: 'images/slide-img3.webp' }
         ],
         timer: false,
         transition: [ 'zoomIn', ],
